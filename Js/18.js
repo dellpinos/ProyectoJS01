c9817d907d95caecfa884c35177e1ba7e46a9871
Martin del Pino <dellpinos7@gmail.com>
2022-12-02 23:49:43 -0300

    Actualizacion Pruebas Js

--- Js/18.js
+++ Js/18.js
@@ -1,2 +1,26 @@
 // Funciones
 
+// Declaracion de la funcion  
+
+function sumar() {
+    console.log( 10 + 10);
+
+}
+
+sumar();  // Gracias al Hoisting de Js puedo poner la llamada antes de la funcion 
+
+
+// Exprension de la funcion
+
+const sumar2 = function() {
+    console.log( 3 + 3);
+}
+
+sumar2();  // Por el Hoisting de Js no puedo poner esta llamada antes de la funcion
+
+// IIFE
+
+(function() {
+    console.log('esto es una funcion');
+})();
+
