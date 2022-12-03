b8cc4aa719ac5b9376ceb2b04079dd3fa71351b0
Martin del Pino <dellpinos7@gmail.com>
2022-12-03 01:39:48 -0300

    Pruebas de Js 01

---
+++ Js/18_2.js
@@ -0,0 +1,16 @@
+
+
+let pepe = "Lololo";
+
+V1 = 18;
+
+V2 = 500;
+
+if (V1 < V2) {
+    pepe = V2
+};
+
+
+
+
+console.log(pepe);
---
+++ Js/PruebaIF_2.js
@@ -0,0 +1,19 @@
+
+
+
+const Valor1 = 20;
+
+const Valor2 = 2;
+
+const Valor3 = 80;
+
+//   if (Valor1 / Valor2 > Valor3) {
+//       console.log("Esto es un Hola Mundo mas realista")
+//   };
+
+if (Valor1 % 5 === 0) {
+    console.log("El Valor1 es multiplo de 5")
+} else {
+    console.log("El Valor1 NO es multiplo de 5")
+};
+
